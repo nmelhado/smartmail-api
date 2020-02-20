4f5dac183e5a3f0060969782e224c041352cb948
Nicholas Melhado <n.melhado@gmail.com>
2020-02-20 13:40:04 -0500

    Finish user tests

--- tests/modeltests/user_model_test.go
+++ tests/modeltests/user_model_test.go
@@ -16,8 +16,8 @@ func TestFindAllUsers(t *testing.T) {
 		log.Fatalf("Error refreshing user table %v\n", err)
 	}
 
-	err = seedTables()
-	if err != nil {
+	_, _, _, seedErr := seedTables()
+	if seedErr != nil {
 		log.Fatalf("Error seeding user table %v\n", err)
 	}
 
@@ -35,24 +35,27 @@ func TestSaveUser(t *testing.T) {
 	if err != nil {
 		log.Fatalf("Error user refreshing table %v\n", err)
 	}
-	models.User{
-			ID: 1,
-			CosmoID: "ABCDEFGH",
-			FirstName: "Test",
-			LastName: "McGee",
-			Phone: "2125478965",
-			Authority: models.UserAuth,
-			Email:    "test@gmail.com",
-			Password: "password!",
-		},
+
+	newUser := models.User{
+		CosmoID:   "ABCDEFGH",
+		FirstName: "Test",
+		LastName:  "McGee",
+		Phone:     "2125478965",
+		Authority: models.UserAuth,
+		Email:     "test@gmail.com",
+		Password:  "password!",
+	}
 	savedUser, err := newUser.SaveUser(server.DB)
 	if err != nil {
 		t.Errorf("Error while saving a user: %v\n", err)
 		return
 	}
-	assert.Equal(t, newUser.ID, savedUser.ID)
+	assert.Equal(t, newUser.CosmoID, savedUser.CosmoID)
+	assert.Equal(t, newUser.FirstName, savedUser.FirstName)
+	assert.Equal(t, newUser.LastName, savedUser.LastName)
+	assert.Equal(t, newUser.Phone, savedUser.Phone)
+	assert.Equal(t, newUser.Authority, savedUser.Authority)
 	assert.Equal(t, newUser.Email, savedUser.Email)
-	assert.Equal(t, newUser.Nickname, savedUser.Nickname)
 }
 
 func TestGetUserByID(t *testing.T) {
@@ -62,18 +65,22 @@ func TestGetUserByID(t *testing.T) {
 		log.Fatalf("Error user refreshing table %v\n", err)
 	}
 
-	user, err := seedTables()
+	users, _, _, err := seedTables()
 	if err != nil {
 		log.Fatalf("cannot seed users table: %v", err)
 	}
-	foundUser, err := userInstance.FindUserByID(server.DB, user.ID)
+	foundUser, err := userInstance.FindUserByID(server.DB, users[0].ID)
 	if err != nil {
 		t.Errorf("this is the error getting one user: %v\n", err)
 		return
 	}
-	assert.Equal(t, foundUser.ID, user.ID)
-	assert.Equal(t, foundUser.Email, user.Email)
-	assert.Equal(t, foundUser.Nickname, user.Nickname)
+	assert.Equal(t, foundUser.ID, users[0].ID)
+	assert.Equal(t, foundUser.CosmoID, users[0].CosmoID)
+	assert.Equal(t, foundUser.FirstName, users[0].FirstName)
+	assert.Equal(t, foundUser.LastName, users[0].LastName)
+	assert.Equal(t, foundUser.Phone, users[0].Phone)
+	assert.Equal(t, foundUser.Authority, users[0].Authority)
+	assert.Equal(t, foundUser.Email, users[0].Email)
 }
 
 func TestUpdateAUser(t *testing.T) {
@@ -83,25 +90,28 @@ func TestUpdateAUser(t *testing.T) {
 		log.Fatal(err)
 	}
 
-	user, err := seedTables()
+	users, _, _, err := seedTables()
 	if err != nil {
 		log.Fatalf("Cannot seed user: %v\n", err)
 	}
 
 	userUpdate := models.User{
-		ID:       1,
-		Nickname: "modiUpdate",
-		Email:    "modiupdate@gmail.com",
-		Password: "password",
+		FirstName: "Al",
+		Email:     "al@gmail.com",
+		Password:  "NotTheJoker!",
 	}
-	updatedUser, err := userUpdate.UpdateAUser(server.DB, user.ID)
+	updatedUser, err := userUpdate.UpdateAUser(server.DB, users[0].ID)
 	if err != nil {
 		t.Errorf("this is the error updating the user: %v\n", err)
 		return
 	}
-	assert.Equal(t, updatedUser.ID, userUpdate.ID)
+	assert.Equal(t, updatedUser.ID, users[0].ID)
+	assert.Equal(t, updatedUser.CosmoID, users[0].CosmoID)
+	assert.Equal(t, updatedUser.FirstName, userUpdate.FirstName)
+	assert.Equal(t, updatedUser.LastName, users[0].LastName)
+	assert.Equal(t, updatedUser.Phone, users[0].Phone)
+	assert.Equal(t, updatedUser.Authority, users[0].Authority)
 	assert.Equal(t, updatedUser.Email, userUpdate.Email)
-	assert.Equal(t, updatedUser.Nickname, userUpdate.Nickname)
 }
 
 func TestDeleteUser(t *testing.T) {
@@ -111,20 +121,18 @@ func TestDeleteUser(t *testing.T) {
 		log.Fatal(err)
 	}
 
-	user, err := seedTables()
+	users, _, _, err := seedTables()
 
 	if err != nil {
 		log.Fatalf("Cannot seed user: %v\n", err)
 	}
 
-	isDeleted, err := userInstance.DeleteUser(server.DB, user.ID)
+	isDeleted, err := userInstance.DeleteUser(server.DB, users[0].ID)
 	if err != nil {
 		t.Errorf("this is the error deleting the user: %v\n", err)
 		return
 	}
-	//one shows that the record has been deleted or:
-	// assert.Equal(t, int(isDeleted), 1)
 
-	//Can be done this way too
+	//one shows that the record has been deleted
 	assert.Equal(t, isDeleted, int64(1))
 }
